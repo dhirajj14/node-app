ddeacb5e766f1bfe15dba832d356337755ef3907
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-10-26 12:54:18 -0500

    update app.js

--- app.js
+++ app.js
@@ -7,32 +7,51 @@ var express = require('express'),
     multerS3 = require('multer-s3');
 
 aws.config.update({
-	accessKeyId: 'Your Amazon S3 Access Key',
-    secretAccessKey: 'Your Amazon S3 Secret Key',    
+// 	accessKeyId: 'Your Amazon S3 Access Key',
+//     secretAccessKey: 'Your Amazon S3 Secret Key',    
     region: 'us-east-1'
 });
 
-var app = express(),
-    s3 = new aws.S3();
+var app = express();
+var s3 = new aws.S3();
 
 app.use(bodyParser.json());
 
-var upload = multer({
+var promise = new Promise(function(resolve, reject) { 
+  s3.listBuckets(function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+   else resolve(data.Buckets[0].Name);           // successful response
+  });
+}).catch((error) => {
+  console.error(error);
+});
+
+var d1 = "";
+promise.then((value) => {
+  var upload = multer({
     storage: multerS3({
         s3: s3,
-        bucket: 'fall2020-dpj',
+        bucket: d1,
         key: function (req, file, cb) {
             cb(null, file.originalname);
-        }
-    })
+            }
+        })
+    });
+    
+    app.post('/upload', upload.array('uploadFile',1), function (req, res, next) {
+    res.send("File uploaded successfully to Amazon S3 Server!");
+    });
+    
+}).catch((error) => {
+  console.error(error);
 });
+    
+
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/upload', upload.array('uploadFile',1), function (req, res, next) {
-    res.send("File uploaded successfully to Amazon S3 Server!");
-});
 
 app.listen(3300, function () {
     console.log('Amazon s3 file upload app listening on port 3300');
